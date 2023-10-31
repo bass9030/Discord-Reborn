9e7aaa0359fce8ac779d3ceb15b318417ba955fc
bass9030 <bass90301.naver.comm@gmail.com>
2023-10-31 23:51:52 +0900

    Add debug build script

--- build_debug.sh
+++ build_debug.sh
@@ -1,13 +1,13 @@
 echo Start build...
-xcodebuild clean build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+xcodebuild clean build -configuration Debug CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
 
 echo Cleaning previous build..
-cd build/Release-iphoneos
+cd build/Debug-iphoneos
 rm -rf Payload
 rm Discord.ipa
 echo Packing build...
 mkdir Payload
 mv Discord.app Payload
-zip -r Discord-Release.ipa Payload
+zip -r Discord-Debug.ipa Payload
 
 echo Build Done!
